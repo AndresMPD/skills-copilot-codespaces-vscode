f25faeb09d62a7f0b049eec202c05c0ad5e3b780
Andrés Mendioroz Peña <94523914+AndresMPD@users.noreply.github.com>
2024-06-06 16:32:51 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,16 @@
+function skillMembers() {
+  return [
+    {
+      name: 'John Doe',
+      skill: 'JavaScript'
+    },
+    {
+      name: 'Jane Doe',
+      skill: 'PHP'
+    },
+    {
+      name: 'Jim Doe',
+      skill: 'Python'
+    }
+  ];
+}
